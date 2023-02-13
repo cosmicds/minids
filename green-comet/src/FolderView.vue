c201b5c1353313c49e646e97608ff5e18635f3ec
Carifio24 <carifio24@gmail.com>
2023-02-13 10:03:31 -0500

    Simplify isMobile.

--- green-comet/src/FolderView.vue
+++ green-comet/src/FolderView.vue
@@ -182,11 +182,7 @@ export default defineComponent({
     },
 
     isMobile() {
-      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        return true
-      } else {
-        return false
-      }
+      return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
     },
   },
 
