2cceb5243a4cdf7afdf892ff3f2558c98417973f
John Arban Lewis <johnarban@users.noreply.github.com>
2023-02-14 12:47:50 -0500

    playing now updates the view independently

--- green-comet/src/C2022E3.vue
+++ green-comet/src/C2022E3.vue
@@ -1842,6 +1842,7 @@ export default defineComponent({
           } else {
             this.selectedTime = minDate;
           }
+          this.$nextTick( () => { this.updateViewForDate() })
         }, 300);
       }
     }
