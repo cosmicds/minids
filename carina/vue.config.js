66c0cb9f0937c147fc1ed7cf4cb618e82924ea98
Jon Carifio <carifio24@gmail.com>
2023-03-03 00:30:34 -0500

    Workaround for Safari invalid host header issue on BrowserStack.

--- carina/vue.config.js
+++ carina/vue.config.js
@@ -8,5 +8,12 @@ module.exports = defineConfig({
     plugins: [
       new VuetifyPlugin()
     ]
+  },
+
+  // Needed for BrowserStack/Safari testing as of 2023 March. This makes the
+  // dev server insecure, but that's OK since we only use it in controlled
+  // circumstances. https://stackoverflow.com/questions/43619644
+  devServer: {
+    allowedHosts: 'all'
   }
 });
