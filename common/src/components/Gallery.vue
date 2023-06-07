25f4d6bbd9f9e35e661d0fdbb21ced23e59e2fd1
Carifio24 <carifio24@gmail.com>
2023-06-07 10:23:03 -0400

    Rewrite gallery using Options API.

--- common/src/components/Gallery.vue
+++ common/src/components/Gallery.vue
@@ -9,7 +9,7 @@
     <v-window-item
       :style="cssVars"
       class="gallery-content"
-      :fullscreen="open && props.fullscreen"
+      :fullscreen="open && fullscreen"
     >
       <div
         v-for="[index, place] of places.entries()"
@@ -23,69 +23,79 @@
   </v-window>
 </template>
 
-<script setup lang="ts">
-import { defineProps, reactive, ref, onBeforeMount, computed } from "vue";
+<script lang="ts">
+import { defineComponent } from 'vue';
 import { Folder, Imageset, Place } from "@wwtelescope/engine";
 import { engineStore } from "@wwtelescope/engine-pinia";
+import { mapActions } from "pinia";
 
-const wwtStore = engineStore();
+export default defineComponent({
+  
+  props: {
+    wtmlUrl: { type: String, required: true },
+    width: { type: String, default: "67%" },
+    height: { type: String, default: "50%" },
+    fullscreen: { type: Boolean, default: false }
+  },
 
-interface GalleryProps {
-  wtmlUrl: string;
-  width: string;
-  height: string;
-  fullscreen: boolean;
-}
+  async created() {
+    this.places = await this.placesFromWtml(this.wtmlUrl);
+  },
 
+  data() {
+    return {
+      open: false,
+      places: [] as Place[]
+    };
+  },
 
-function getImageset(place: Place): Imageset | null {
-  return place.get_studyImageset() ?? place.get_backgroundImageset();
-}
+  methods: {
+    ...mapActions(engineStore, ["loadImageCollection"]),
 
+    getImageset(place: Place): Imageset | null {
+      return place.get_backgroundImageset() ?? place.get_studyImageset();
+    },
 
-function extractPlaces(folder: Folder): Place[] {
-  let places: Place[] = [];
-  for (const child of folder.get_children() ?? []) {
-    if (child instanceof Place) {
-      const iset = getImageset(child);
-      if (iset !== null) {
-        places.push(child);
+    extractPlaces(folder: Folder): Place[] {
+      let places: Place[] = [];
+      for (const child of folder.get_children() ?? []) {
+        if (child instanceof Place) {
+          const iset = this.getImageset(child);
+          if (iset !== null) {
+            places.push(child);
+          }
+        } else if (child instanceof Folder) {
+          places = places.concat(this.extractPlaces(child));
+        }
       }
-    } else if (child instanceof Folder) {
-      places = places.concat(extractPlaces(child));
+      return places;
+    },
+
+    async placesFromWtml(wtmlUrl: string): Promise<Place[]> {
+      return this.loadImageCollection({
+        url: wtmlUrl,
+        loadChildFolders: true
+      }).then((folder) => this.extractPlaces(folder));  
+    }
+  },
+
+  computed: {
+    cssVars() {
+      return {
+        "--width": this.width,
+        "--height": this.height
+      };
     }
   }
-  return places;
-}
-
-async function placesFromWtml(wtmlUrl: string): Promise<Place[]> {
-  return wwtStore.loadImageCollection({
-    url: wtmlUrl,
-    loadChildFolders: true
-  }).then((folder) => extractPlaces(folder));
-}
-
-const cssVars = computed(() => {
-  return {
-    "--width": props.width,
-    "--height": props.height
-  };
 });
 
-const open = ref(false);
-const props = defineProps<GalleryProps>();
-
-let places = reactive<Place[]>([]);
-
-onBeforeMount(async () => {
-  places = await placesFromWtml(props.wtmlUrl);
-});
 </script>
 
-<style scoped>
+<style>
 .gallery-content {
   background: transparent;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(6px);
 }
 </style>
+
--- common/src/index.ts
+++ common/src/index.ts
@@ -1,10 +1,12 @@
 import { KeyboardControlSettings } from "./keyboard";
 import MiniDSBase from "./components/MiniDSBase";
 import { BackgroundImageset, skyBackgroundImagesets } from "./background";
+import Gallery from "./components/Gallery.vue";
 
 export {
   BackgroundImageset,
   KeyboardControlSettings,
   MiniDSBase,
+  Gallery,
   skyBackgroundImagesets
 };
