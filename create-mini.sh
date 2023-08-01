131f405f8419290e07a3c7321b4f321fdad66a3a
Carifio24 <carifio24@gmail.com>
2023-08-01 17:27:27 -0400

    Start working on script for creating a new mini.

---
+++ create-mini.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO(?): Make a PowerShell version of this
+
+if [[ $# -ne 1 ]]; then
+    echo "Should only specify one parameter!"
+    exit 2
+fi
+
+name=$1
+mkdir -p $1
+
+# Add the new story as a workspace in package.json
+cat package.json |
+    jq --arg name $name '.workspaces[.workspaces | length] |= . + $name' | jq '.workspaces |= if . then sort else empty end' > tmp.json
+mv tmp.json package.json
