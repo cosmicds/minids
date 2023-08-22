1983a1e43b5348883058a861c5a5f76012f55b56
Carifio24 <carifio24@gmail.com>
2023-08-22 11:44:52 -0400

    Add setup for text and video, and update page title.

--- radwave/src/Radwave.vue
+++ radwave/src/Radwave.vue
@@ -67,6 +67,135 @@
     <div class="bottom-content">
     </div>
 
+    <v-dialog
+      id="video-container"
+      v-model="showVideoSheet"
+      transition="slide-y-transition"
+      fullscreen
+    >
+      <div class="video-wrapper">
+        <font-awesome-icon
+          id="video-close-icon"
+          class="close-icon"
+          icon="times"
+          size="lg"
+          @click="showVideoSheet = false"
+          @keyup.enter="showVideoSheet = false"
+          tabindex="0"
+        ></font-awesome-icon>
+        <video
+          controls
+          id="info-video"
+        >
+          <source src="" type="video/mp4">
+        </video>
+      </div>
+    </v-dialog>
+
+    <v-dialog
+      :style="cssVars"
+      class="bottom-sheet"
+      id="text-bottom-sheet"
+      hide-overlay
+      persistent
+      no-click-animation
+      absolute
+      width="100%"
+      :scrim="false"
+      location="bottom"
+      v-model="showTextSheet"
+      transition="dialog-bottom-transition"
+    >
+      <v-card height="100%">
+        <v-tabs
+          v-model="tab"
+          height="32px"
+          :color="accentColor"
+          :slider-color="accentColor"
+          id="tabs"
+          dense
+          grow
+        >
+          <v-tab tabindex="0"><h3>Information</h3></v-tab>
+          <v-tab tabindex="0"><h3>Using WWT</h3></v-tab>
+        </v-tabs>
+        <font-awesome-icon
+          id="close-text-icon"
+          class="control-icon"
+          icon="times"
+          size="lg"
+          @click="showTextSheet = false"
+          @keyup.enter="showTextSheet = false"
+          tabindex="0"
+        ></font-awesome-icon>
+        <v-window v-model="tab" id="tab-items" class="pb-2 no-bottom-border-radius">
+          <v-window-item>
+            <v-card class="no-bottom-border-radius scrollable">
+              <v-card-text class="info-text no-bottom-border-radius">
+                Information goes here
+              </v-card-text>
+            </v-card>
+          </v-window-item>
+          <v-window-item>
+            <v-card class="no-bottom-border-radius scrollable">
+              <v-card-text class="info-text no-bottom-border-radius">
+                <v-container>
+                  <v-row align="center">
+                  <v-col cols="4">
+                      <v-chip
+                        label
+                        outlined
+                      >
+                        Pan
+                      </v-chip>
+                    </v-col>
+                    <v-col cols="8" class="pt-1">
+                      <strong>{{ touchscreen ? "press + drag" : "click + drag" }}</strong>  {{ touchscreen ? ":" : "or" }}  <strong>{{ touchscreen ? ":" : "W-A-S-D" }}</strong> {{ touchscreen ? ":" : "keys" }}<br>
+                    </v-col>
+                  </v-row>
+                  <v-row align="center">
+                    <v-col cols="4">
+                      <v-chip
+                        label
+                        outlined
+                      >
+                        Zoom
+                      </v-chip>
+                    </v-col>
+                    <v-col cols="8" class="pt-1">
+                      <strong>{{ touchscreen ? "pinch in and out" : "scroll in and out" }}</strong> {{ touchscreen ? ":" : "or" }} <strong>{{ touchscreen ? ":" : "I-O" }}</strong> {{ touchscreen ? ":" : "keys" }}<br>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12">
+                      <div class="credits">
+                      <h3>Credits:</h3>
+                      <h4><a href="https://www.cosmicds.cfa.harvard.edu/" target="_blank" rel="noopener noreferrer">CosmicDS</a> Mini Stories Team:</h4>
+                      John Lewis<br>
+                      Jon Carifio<br>
+                      Pat Udomprasert<br>
+                      Alyssa Goodman<br>
+                      Mary Dussault<br>
+                      Harry Houghton<br>
+                      Anna Nolin<br>
+                      Evaluator: Sue Sunbury<br>
+                      <br>
+                      <h4>WorldWide Telescope Team:</h4>
+                      Peter Williams<br>
+                      A. David Weigel<br>
+                      Jon Carifio<br>
+                      </div>
+                      <v-spacer class="end-spacer"></v-spacer>
+                    </v-col>
+                  </v-row>
+                </v-container>              
+              </v-card-text>
+            </v-card>
+          </v-window-item>
+        </v-window>
+      </v-card>
+    </v-dialog>
+
   </div>
 </v-app>
 </template>
