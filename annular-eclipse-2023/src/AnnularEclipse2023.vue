9c3b50043bbbbd8fb8f24497e4e9b0aaa5f7db1b
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-08-28 16:00:02 -0400

    Run template from #146 to create/initialize content

---
+++ annular-eclipse-2023/src/AnnularEclipse2023.vue
@@ -0,0 +1,623 @@
+<template>
+<v-app
+  id="app"
+  :style="cssVars"
+>
+  <div
+    id="main-content"
+  >
+    <WorldWideTelescope
+      :wwt-namespace="wwtNamespace"
+    ></WorldWideTelescope>
+
+    <v-overlay
+      :model-value="showSplashScreen"
+      absolute
+      opacity="0.6"
+      :style="cssVars"
+      id="splash-overlay"
+    >
+      <div
+        id="splash-screen"
+        v-click-outside="closeSplashScreen"
+        :style="cssVars"
+      >
+        Splash Screen
+      </div>
+    </v-overlay>
+
+    <transition name="fade">
+      <div
+        class="modal"
+        id="modal-loading"
+        v-show="isLoading"
+      >
+        <div class="container">
+          <div class="spinner"></div>
+          <p>Loading …</p>
+        </div>
+      </div>
+    </transition>
+
+    <div class="top-content">
+      <div id="left-buttons">
+        <icon-button
+          v-model="showTextSheet"
+          fa-icon="book-open"
+          :color="accentColor"
+          :tooltip-text="showTextSheet ? 'Hide Info' : 'Learn More'"
+          tooltip-location="start"
+        >
+        </icon-button>
+        <icon-button
+          v-model="showVideoSheet"
+          fa-icon="video"
+          :color="accentColor"
+          tooltip-text="Watch video"
+          tooltip-location="start"
+        >
+        </icon-button>
+      </div>
+      <div id="center-buttons">
+      </div>
+      <div id="right-buttons">
+      </div>
+    </div>
+
+    <div class="bottom-content">
+    </div>
+
+    <v-dialog
+      id="video-container"
+      v-model="showVideoSheet"
+      transition="slide-y-transition"
+      fullscreen
+    >
+      <div class="video-wrapper">
+        <font-awesome-icon
+          id="video-close-icon"
+          class="close-icon"
+          icon="times"
+          size="lg"
+          @click="showVideoSheet = false"
+          @keyup.enter="showVideoSheet = false"
+          tabindex="0"
+        ></font-awesome-icon>
+        <video
+          controls
+          id="info-video"
+        >
+          <source src="" type="video/mp4">
+        </video>
+      </div>
+    </v-dialog>
+
+    <v-dialog
+      :style="cssVars"
+      class="bottom-sheet"
+      id="text-bottom-sheet"
+      hide-overlay
+      persistent
+      no-click-animation
+      absolute
+      width="100%"
+      :scrim="false"
+      location="bottom"
+      v-model="showTextSheet"
+      transition="dialog-bottom-transition"
+    >
+      <v-card height="100%">
+        <v-tabs
+          v-model="tab"
+          height="32px"
+          :color="accentColor"
+          :slider-color="accentColor"
+          id="tabs"
+          dense
+          grow
+        >
+          <v-tab tabindex="0"><h3>Information</h3></v-tab>
+          <v-tab tabindex="0"><h3>Using WWT</h3></v-tab>
+        </v-tabs>
+        <font-awesome-icon
+          id="close-text-icon"
+          class="control-icon"
+          icon="times"
+          size="lg"
+          @click="showTextSheet = false"
+          @keyup.enter="showTextSheet = false"
+          tabindex="0"
+        ></font-awesome-icon>
+        <v-window v-model="tab" id="tab-items" class="pb-2 no-bottom-border-radius">
+          <v-window-item>
+            <v-card class="no-bottom-border-radius scrollable">
+              <v-card-text class="info-text no-bottom-border-radius">
+                Information goes here
+              </v-card-text>
+            </v-card>
+          </v-window-item>
+          <v-window-item>
+            <v-card class="no-bottom-border-radius scrollable">
+              <v-card-text class="info-text no-bottom-border-radius">
+                <v-container>
+                  <v-row align="center">
+                  <v-col cols="4">
+                      <v-chip
+                        label
+                        outlined
+                      >
+                        Pan
+                      </v-chip>
+                    </v-col>
+                    <v-col cols="8" class="pt-1">
+                      <strong>{{ touchscreen ? "press + drag" : "click + drag" }}</strong>  {{ touchscreen ? ":" : "or" }}  <strong>{{ touchscreen ? ":" : "W-A-S-D" }}</strong> {{ touchscreen ? ":" : "keys" }}<br>
+                    </v-col>
+                  </v-row>
+                  <v-row align="center">
+                    <v-col cols="4">
+                      <v-chip
+                        label
+                        outlined
+                      >
+                        Zoom
+                      </v-chip>
+                    </v-col>
+                    <v-col cols="8" class="pt-1">
+                      <strong>{{ touchscreen ? "pinch in and out" : "scroll in and out" }}</strong> {{ touchscreen ? ":" : "or" }} <strong>{{ touchscreen ? ":" : "I-O" }}</strong> {{ touchscreen ? ":" : "keys" }}<br>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12">
+                      <div class="credits">
+                      <h3>Credits:</h3>
+                      <h4><a href="https://www.cosmicds.cfa.harvard.edu/" target="_blank" rel="noopener noreferrer">CosmicDS</a> Mini Stories Team:</h4>
+                      John Lewis<br>
+                      Jon Carifio<br>
+                      Pat Udomprasert<br>
+                      Alyssa Goodman<br>
+                      Mary Dussault<br>
+                      Harry Houghton<br>
+                      Anna Nolin<br>
+                      Evaluator: Sue Sunbury<br>
+                      <br>
+                      <h4>WorldWide Telescope Team:</h4>
+                      Peter Williams<br>
+                      A. David Weigel<br>
+                      Jon Carifio<br>
+                      </div>
+                      <v-spacer class="end-spacer"></v-spacer>
+                    </v-col>
+                  </v-row>
+                </v-container>              
+              </v-card-text>
+            </v-card>
+          </v-window-item>
+        </v-window>
+      </v-card>
+    </v-dialog>
+
+  </div>
+</v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { MiniDSBase, BackgroundImageset, skyBackgroundImagesets } from "@minids/common";
+import { GotoRADecZoomParams } from "@wwtelescope/engine-pinia";
+
+type SheetType = "text" | "video" | null;
+
+export default defineComponent({
+  extends: MiniDSBase,
+  
+  props: {
+    wwtNamespace: {
+      type: String,
+      required: true
+    },
+    initialCameraParams: {
+      type: Object as PropType<Omit<GotoRADecZoomParams, 'instant'>>,
+      default() {
+        return {
+          raRad: 0,
+          decRad: 0,
+          zoomDeg: 60
+        };
+      }
+    }
+  },
+  data() {
+    return {
+      showSplashScreen: true,
+      backgroundImagesets: [] as BackgroundImageset[],
+      sheet: null as SheetType,
+      layersLoaded: false,
+      positionSet: false,
+      
+      accentColor: "#ffffff",
+
+      tab: 0
+    };
+  },
+
+  mounted() {
+    this.waitForReady().then(async () => {
+      
+      this.backgroundImagesets = [...skyBackgroundImagesets];
+
+      this.gotoRADecZoom({
+        ...this.initialCameraParams,
+        instant: true
+      }).then(() => this.positionSet = true);
+
+      // If there are layers to set up, do that here!
+      this.layersLoaded = true;
+
+    });
+  },
+
+  computed: {
+    ready(): boolean {
+      return this.layersLoaded && this.positionSet;
+    },
+    isLoading(): boolean {
+      return !this.ready;
+    },
+    smallSize(): boolean {
+      return this.$vuetify.display.smAndDown;
+    },
+    mobile(): boolean {
+      return this.smallSize && this.touchscreen;
+    },
+    cssVars() {
+      return {
+        '--accent-color': this.accentColor,
+        '--app-content-height': this.showTextSheet ? '66%' : '100%',
+      };
+    },
+    showTextSheet: {
+      get(): boolean {
+        return this.sheet === 'text';
+      },
+      set(_value: boolean) {
+        this.selectSheet('text');
+      }
+    },
+    showVideoSheet: {
+      get(): boolean {
+        return this.sheet === "video";
+      },
+      set(value: boolean) {
+        this.selectSheet('video');
+        if (!value) {
+          const video = document.querySelector("#info-video") as HTMLVideoElement;
+          video.pause();
+        }
+      }
+    }
+  },
+
+  methods: {
+    closeSplashScreen() {
+      this.showSplashScreen = false; 
+    },
+
+    selectSheet(name: SheetType) {
+      if (this.sheet === name) {
+        this.sheet = null;
+        this.$nextTick(() => {
+          this.blurActiveElement();
+        });
+      } else {
+        this.sheet = name;
+      }
+    }
+  }
+});
+</script>
+
+<style lang="less">
+@font-face {
+  font-family: "Highway Gothic Narrow";
+  src: url("../../assets/HighwayGothicNarrow.ttf");
+}
+
+html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #000;
+
+  overflow: hidden;
+  -ms-overflow-style: none;
+
+  // We don't want a scrollbar for the overall canvas
+  scrollbar-width: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+body {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+}
+
+#main-content {
+  position: fixed;
+  width: 100%;
+  height: var(--app-content-height);
+  overflow: hidden;
+
+  transition: height 0.1s ease-in-out;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  overflow: hidden;
+  font-size: 11pt;
+
+  .wwtelescope-component {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    border-style: none;
+    border-width: 0;
+    margin: 0;
+    padding: 0;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.modal {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#modal-loading {
+  background-color: #000;
+  .container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    .spinner {
+      background-image: url("../../assets/lunar_loader.gif");
+      background-repeat: no-repeat;
+      background-size: contain;
+      width: 3rem;
+      height: 3rem;
+    }
+    p {
+      margin: 0 0 0 1rem;
+      padding: 0;
+      font-size: 150%;
+    }
+  }
+}
+
+#modal-readytostart {
+  cursor: pointer;
+  color: #999;
+
+  &:hover {
+    color: #2aa5f7;
+  }
+
+  div {
+    margin: 0;
+    padding: 0;
+    background-image: url("../../assets/wwt_globe_bg.png");
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    width: 20rem;
+    height: 20rem;
+    max-width: 70%;
+    max-height: 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .icon {
+      width: 60%;
+      height: 60%;
+      margin-left: 14%;
+      margin-top: 3%;
+    }
+  }
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+.control-icon {
+  pointer-events: auto;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+}
+
+.top-content {
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  width: calc(100% - 2rem);
+  pointer-events: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.bottom-content {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+  width: calc(100% - 2rem);
+  pointer-events: none;
+  align-items: center;
+  gap: 5px;
+}
+
+#left-buttons, #right-buttons {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+#splash-overlay {
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  font-size: min(8vw, 7vh);
+}
+
+#splash-screen {
+  max-height: calc(min(90vh, 2040px));
+  max-width: 90vw;
+  background-color: black;
+  backdrop-filter: blur(5px);
+  justify-content: space-around;
+  align-content: center;
+
+  border-radius: 10%;
+  border: min(1.2vw, 0.9vh) solid var(--accent-color);
+  overflow: auto;
+  font-family: 'Highway Gothic Narrow', 'Roboto', sans-serif;
+}
+
+.video-wrapper {
+  height: 100%;
+  background: black;
+  text-align: center;
+  z-index: 1000;
+}
+
+video {
+  height: 100%;
+  width: auto;
+  max-width: 100%;
+  object-fit: contain;
+}
+
+#video-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  overflow: hidden;
+  padding: 0px;
+  z-index: 1000;
+}
+
+.bottom-sheet {
+  .v-overlay__content {
+    align-self: flex-end;
+    padding: 0;
+    margin: 0;
+    max-width: 100%;
+    height: 34%;
+  }
+}
+
+#tabs {
+  width: calc(100% - 3em);
+  align-self: left;
+}
+
+.info-text {
+  height: 33vh;
+  padding-bottom: 25px;
+
+  & a {
+    text-decoration: none;
+  }
+}
+
+.close-icon {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 15;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:focus {
+    color: white;
+    border: 2px solid white;
+  }
+}
+
+.scrollable {
+  overflow-y: auto;
+}
+
+.no-bottom-border-radius {
+  border-bottom-left-radius: 0px !important;
+  border-bottom-right-radius: 0px !important;
+}
+
+#tab-items {
+  // padding-bottom: 2px !important;
+
+  .v-card-text {
+    font-size: ~"max(14px, calc(0.7em + 0.3vw))";
+    padding-top: ~"max(2vw, 16px)";
+    padding-left: ~"max(4vw, 16px)";
+    padding-right: ~"max(4vw, 16px)";
+
+    .end-spacer {
+      height: 25px;
+    }
+  }
+
+}
+
+#close-text-icon {
+  position: absolute;
+  top: 0.25em;
+  right: calc((3em - 0.6875em) / 3); // font-awesome-icons have width 0.6875em
+  color: white;
+}
+
+// This prevents the tabs from having some extra space to the left when the screen is small
+// (around 400px or less)
+.v-tabs:not(.v-tabs--vertical).v-tabs--right>.v-slide-group--is-overflowing.v-tabs-bar--is-mobile:not(.v-slide-group--has-affixes) .v-slide-group__next, .v-tabs:not(.v-tabs--vertical):not(.v-tabs--right)>.v-slide-group--is-overflowing.v-tabs-bar--is-mobile:not(.v-slide-group--has-affixes) .v-slide-group__prev {
+  display: none;
+}
+</style>
