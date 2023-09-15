82d00a3da245f90399b90da7e70e328f5ec2c7e0
Carifio24 <carifio24@gmail.com>
2023-09-15 16:12:54 -0400

    Make patch script more robust.

--- annular-eclipse-2023/patch.sh
+++ annular-eclipse-2023/patch.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-filepath=../node_modules/@wwtelescope/engine/src/index.js
-line=$(head -n 48377 ${filepath} | tail -1 | xargs)
-if [[ $line = TileCache* ]]
+filepath=node_modules/@wwtelescope/engine/src/index.js
+if ! test -f ${filepath}; then
+    filepath="../${filepath}"
+fi
+
+line_number=$(sed -n "/TileCache: /=" ${filepath})
+if [[ ! -z ${line_number} ]]
 then
-    sed -i.bak '48389a      TileCache: [ TileCache, TileCache$, null ],' ${filepath}
-    sed -i.bak '48377d' ${filepath}
-    rm ${filepath}.bak
+    line=$(head -n ${line_number} ${filepath} | tail -1 | xargs)
+    insert_number=$(sed -n "/ScaleTypes: ScaleTypes/=" ${filepath})
+    if [[ $(($line_number < $insert_number)) ]]
+    then
+        sed -i.bak "${insert_number}a      TileCache: [ TileCache, TileCache$, null ]," ${filepath}
+        sed -i.bak "${line_number}d" ${filepath}
+        rm ${filepath}.bak
+    fi
 fi
