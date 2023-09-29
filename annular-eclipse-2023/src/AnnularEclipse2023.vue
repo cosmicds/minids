162137c248582f2cd7887050ab3a1351d60fddc3
John Arban Lewis <johnarban@users.noreply.github.com>
2023-09-29 17:04:06 -0400

    add attribution to choose mode map

--- annular-eclipse-2023/src/AnnularEclipse2023.vue
+++ annular-eclipse-2023/src/AnnularEclipse2023.vue
@@ -867,12 +867,15 @@ export default defineComponent({
         templateUrl: "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}",
         minZoom: 1,
         maxZoom: 16,
-        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
         ext: 'jpg',
         ...initialView
       },
 
-      userSelectedMapOptions: queryData ? { ...queryData, initialZoom: 5 } : initialView,
+      userSelectedMapOptions: {
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+        ...(queryData ? { ...queryData, initialZoom: 5 } : initialView)
+      },
 
       eclipsePathLocations: {
         "Albuquerque, NM": {
