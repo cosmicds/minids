1350b01f2f0dfcb8a203923b9f2798cf11023667
Jon Carifio <carifio24@gmail.com>
2023-10-02 01:07:18 -0400

    Lock roll angle to zero to work around bug in world <--> screen transformations.

--- annular-eclipse-2023/src/AnnularEclipse2023.vue
+++ annular-eclipse-2023/src/AnnularEclipse2023.vue
@@ -2142,6 +2142,18 @@ export default defineComponent({
       this.updateMoonTexture();
     },
 
+    wwtRollRad(angle: number) {
+      if (angle !== 0) {
+        this.gotoRADecZoom({
+          raRad: this.wwtRARad,
+          decRad: this.wwtDecRad,
+          zoomDeg: this.wwtZoomDeg,
+          rollRad: 0,
+          instant: true
+        });
+      }
+    },
+
     useRegularMoon(_show: boolean) {
       this.updateMoonTexture();
     },
