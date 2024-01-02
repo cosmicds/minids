502def0d2315faa3fb1a52501be8cf1c01a7e440
Carifio24 <carifio24@gmail.com>
2024-01-02 16:22:53 +0000

    Make WWT patch idempotent and run on serve.

--- radwave/patch.sh
+++ radwave/patch.sh
@@ -5,7 +5,16 @@ if ! test -f ${filepath}; then
     filepath="../${filepath}"
 fi
 
-reexport_line=$(sed -n "/ ss: () => (\/\* reexport/=" $filepath)
+# Check whether the patch has already been run
+# If so, no need to continue
+altered_line=$(sed -n "/__WEBPACK_IMPORTED_MODULE_X/=" ${filepath} | head -n 1)
+if [[ ! -z ${altered_line} ]]
+then
+    echo "Altered line found!"
+    exit 0
+fi
+
+reexport_line=$(sed -n "/ ss: () => (\/\* reexport/=" ${filepath})
 reexport_line=$((${reexport_line} - 1))
 if [[ ! -z ${reexport_line} ]]
 then
